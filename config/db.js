e55d0c2ec1b122f9b1166092cec9f8032f34e771
amartouqeer <amarwork04@gmail.com>
2026-05-05 23:42:53 +0500

    change db config

--- config/db.js
+++ config/db.js
@@ -4,10 +4,10 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-    console.log(`📊 Database: ${conn.connection.name}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`Database: ${conn.connection.name}`);
   } catch (error) {
-    console.error(`❌ MongoDB Connection Error: ${error.message}`);
+    console.error(` MongoDB Connection Error: ${error.message}`);
     process.exit(1);
   }
 };
